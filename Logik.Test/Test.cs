ce4ed9210531473e647655b818d845796b2b8c49
Puscasu <56638053+andrei-puscasu@users.noreply.github.com>
2025-08-27 14:56:58 +0300

    .

--- Logik.Test/Test.cs
+++ Logik.Test/Test.cs
@@ -2,7 +2,7 @@
 {
     public class Test
     {
-        public static string api = "https://admin.elogik.ro/api/";
+        public static string api = "https://admin.elogik.ro/api/StationSettings";
         
         //StationSettings
         public static string stationSettings = api + "StationSettings";
@@ -10,14 +10,22 @@
         //OrderHeaders(Comenzi) - orderstatus 1 - Deschis, 2 - Platit, 3 - Anulat
         public static string orderHeaders = api + "Rechemare/date=2025-08-23&showAll=true";
 
-        //OrderPayments(Plati) - by orderId
+        //OrderPayments(Plati) - by orderId - PaymentMethod 1 - Cash, 9 - Card
         public static string orderPayments = api + "OrderPayments/orderId=";
 
-        //OrderTransactions(Tranzactii) - by orderId
+        //OrderTransactions(Tranzactii) - by orderId - TransactionStatus - 2 - Anulat, 1 - Valid
         public static string orderTransactions = api + "OrderTransactions/getOrderID=";
 
         //MenuItems
         public static string menuitems = api + "MenuItems";
+
+        /*
+            Raport Vanzari pe Statie
+            
+            Total pe statie, Total CASH pe statie, Total CARD pe statie
+
+            Produse cumulate pe statie (nume, cantitate, total)
+        */
 
         HttpClient client;
         public Test()
